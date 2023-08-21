68d724685502ac1531e0cc3fdc211e530ed5b387
danialkarim807 <danialkarim8@gmail.com>
2023-08-21 23:31:24 +0500

    third commit for script read

--- mavenProject.groovy
+++ mavenProject.groovy
@@ -7,7 +7,7 @@ job("first-maven-project-by-DSL") {
         scm("* * * * *")
     }
     steps {
-        maven("clean package", "pom.xml")
+        maven("clean package", "maven/mavenProject.groovy")
     }
     publishers {
         archiveArtifacts "**/*.war"
